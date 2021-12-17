d0dfd521bc81bc43139e4c16571fd9a513b947db
Mario Silva <mariocsilva119@gmail.com>
2021-12-17 22:24:53 +0000

    adjusted tests prints

--- src/tests.py
+++ src/tests.py
@@ -5,9 +5,9 @@ import time
 
 
 class Test():
-    def __init__(self, fname="datasets/it_book.txt", repetitions = 1000, k=10):
+    def __init__(self, fname="datasets/it_book.txt", rep=1000, k=10):
         self.fname = fname
-        self.repetitions = repetitions
+        self.rep = rep
         self.k = k
 
         self.run_test()
@@ -27,7 +27,7 @@ class Test():
 
 
     def most_frequent(self, counter, total_top_k_letters):
-        return {letter: counter.estimated_events(occur / self.repetitions) for letter, occur in \
+        return {letter: counter.estimated_events(occur / self.rep) for letter, occur in \
             sorted(total_top_k_letters.items(), key=lambda x: x[1], reverse=True)[:self.k]}
 
 
@@ -39,7 +39,7 @@ class Test():
         total_estimated_events = 0
         total_top_k_letters = {}
 
-        for _ in range(self.repetitions):
+        for _ in range(self.rep):
             tic = time.time()
             counter.count()
             total_time += time.time() - tic
@@ -49,23 +49,23 @@ class Test():
                 total_estimated_events += counter.estimated_events()
                 total_top_k_letters = self.merge_and_add_dicts(total_top_k_letters, counter.letter_occur)
 
-        avg_time = total_time / self.repetitions
+        avg_time = total_time / self.rep
 
-        print(f"\tAverage Counting Time: {avg_time:.2f} seconds")
+        print(f"\t{'Average ' if self.k != 1 else ''}Counting Time: {avg_time:.2f} seconds")
 
         if not exact:
-            avg_events = total_events / self.repetitions
-            estimated_avg_events = total_estimated_events / self.repetitions
+            avg_events = total_events / self.rep
+            estimated_avg_events = total_estimated_events / self.rep
             common_top_k_letters = self.most_frequent(counter, total_top_k_letters)
 
-            print(f"\tAverage Counted Events: {avg_events}")
-            print(f"\tAverage Estimated Number of Events: {estimated_avg_events}")
-            print(f"\tMost Frequent Top {self.k} Letters:")
-            [print(f"\t\tAverage Events of the Letter '{letter}': {occur}") for letter, occur in common_top_k_letters.items()]
+            print(f"\t{'Average ' if self.rep != 1 else ''}Counted Events: {avg_events:.2f}")
+            print(f"\t{'Average ' if self.rep != 1 else ''}Estimated Number of Events: {estimated_avg_events:.2f}")
+            print(f"\t{'Most Frequent ' if self.rep != 1 else ''}Top {self.k} Letters:")
+            [print(f"\t\t{'Average ' if self.rep != 1 else ''}Events of the Letter '{letter}': {occur}") for letter, occur in common_top_k_letters.items()]
             print()
             return 
 
         print(f"\tCounted Events: {counter.counter_value}")
         print(f"\tTop {self.k} Letters:")
-        [print(f"\t\tAverage Events of the Letter '{letter}': {occur}") for letter, occur in counter.top_k_letters(self.k).items()]
+        [print(f"\t\tEvents of the Letter '{letter}': {occur}") for letter, occur in counter.top_k_letters(self.k).items()]
         print()
